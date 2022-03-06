5c6c80a354a2a96e63592c3aca4d12aa9fbc7d14
deemashahwan <dshahwan42@gmail.com>
2022-03-06 18:10:00 +0300

    .

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,5 @@ const authController=require('../Controllers/auth')
 const router=express.Router()
 
 router.post("/sign_up",authController.register)
-router.post("/index",authController.register)
-
 
 module.exports=router
