3e2699fe3e9f4b1c8ba3187e86de9e5667870e8d
deemashahwan <dshahwan42@gmail.com>
2022-04-15 22:03:57 +0300

    .

--- controllers/auth3.js
+++ controllers/auth3.js
@@ -16,27 +16,38 @@ exports.register=(req,res)=>{
 
     console.log(req.body)
     const{Date,Time,customers,Note}=req.body
-
-    db.query('SELECT * FROM tables WHERE date=? AND time=?',[Date,Time],async(err,results)=>{
+   
+    db.query('SELECT date,time FROM tables WHERE date=? AND time=?',[Date,Time],async(err,results)=>{
         if(err){
             console.log(err)
         }
-       
+        console.log(results)
+        if(results[0].length > 1 && results[1].length >1){
+            return res.render('Booking',{
+                message:'The restaurant is full at this time'
+
+            })
+        }
+        else{
         db.query('INSERT INTO tables SET?',{date:Date,time:Time,num_of_people:customers,note:Note},(err,results)=>{
-            if(err){
-                console.log(err)
-            }
-            else{
-                console.log(results)
-            }
-            res.render("menu")
-                });
-                
+                if(err){
+                    console.log(err)
+                }
+                else{
+                    console.log(results)
+                }
+                    
+        res.render("menu")
+        });
+       } 
+
+       
+    });           
 
 
 
 
-    });
+    
 
 
 }
