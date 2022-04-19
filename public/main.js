954ef33d7a76241c5de9f8b907ef512966c2b117
Zaina202 <92650788+Zaina202@users.noreply.github.com>
2022-04-19 07:05:27 +0300

    .

--- public/main.js
+++ public/main.js
@@ -1,12 +1,13 @@
 
 
 
-    // const togglePassword=document.querySelector("#togglePassword");
-    // const password=document.querySelector("#password");
-    // togglePassword.addEventListener("click",function(){ 
-    //     const type=password.getAttribute("type")==="password" ? "text":"password";
-    //     password.setAttribute("type",type);
-    //     this.classList.toggle("fa-eye-slash");
+    const togglePassword=document.querySelector("#togglePassword");
+    const password=document.querySelector("#password");
+    togglePassword.addEventListener("click",function(){ 
+        const type=password.getAttribute("type")==="password" ? "text":"password";
+        password.setAttribute("type",type);
+        this.classList.toggle("fa-eye-slash");
+    })
 
 
 // $(".toggle-password").click(function(){
