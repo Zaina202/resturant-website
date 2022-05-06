15bcc700972b2b1de8819fe1f77743ac701694fe
deemashahwan <dshahwan42@gmail.com>
2022-05-06 17:29:37 +0300

    .

--- controllers/auth3.js
+++ controllers/auth3.js
@@ -15,7 +15,7 @@ const async = require("hbs/lib/async")
 exports.register=(req,res)=>{
 
     console.log(req.body)
-    const{Date,Time,customers,Note}=req.body
+    const{Date,Time,customers,num,Note}=req.body
    
     db.query('SELECT * FROM tables WHERE  time=?',[Time],(err,results)=>{
       if(err){
@@ -34,13 +34,13 @@ exports.register=(req,res)=>{
                            
             
             }
-        if(Date===""||Time===""||customers===""){
+        if(Date===""||Time===""||num===""||customers===""){
             return res.render('Booking',{
-                message:'plese enter values'   
+                message:'plese enter your information'   
             })
         }
          else{
-         db.query('INSERT INTO tables SET?',{date:Date,time:Time,num_of_people:customers,note:Note},(err,results)=>{
+         db.query('INSERT INTO tables SET?',{date:Date,time:Time,num_of_people:customers,phone_num:num,note:Note},(err,results)=>{
                  if(err){
                      console.log(err)
                  }
