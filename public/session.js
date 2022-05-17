8d73ba282a7745420fb31635ee81f85e13bee967
Zaina202 <92650788+Zaina202@users.noreply.github.com>
2022-05-17 03:21:28 +0300

    .

--- public/session.js
+++ public/session.js
@@ -1,10 +1,44 @@
-const myObject={
-    name: "Dave",
-    hobbies:["eat", "sleep", "code"],
-    logName: function (){
-       console.log(this.name);}
-  };
-  const myArray= ["eat", "sleep", "code"];
-  sessionStorage.setItem("mySessionStore",JSON.stringify(myArray));
-  const mySessionData=JSON.parse(sessionStorage.getItem ("mySessionStore"));
-  console.log(mySessionData);
+// const myObject={
+//     name: "Dave",
+//     hobbies:["eat", "sleep", "code"],
+//     logName: function (){
+//        console.log(this.name);}
+//   };
+//   const myArray= ["eat", "sleep", "code"];
+//   sessionStorage.setItem("mySessionStore",JSON.stringify(myArray));
+//   const mySessionData=JSON.parse(sessionStorage.getItem ("mySessionStore"));
+//   console.log(mySessionData);
+var interval;
+         $(document).on('mousemove', function () {
+             clearInterval(interval);
+             var coutdown = 1 * 60, $timer = $('.timer'); // After 5 minutes session expired  (mouse button click code)
+             $timer.text(coutdown);
+             interval = setInterval(function () {
+                 $timer.text(--coutdown);
+
+                 if (coutdown === 0) {
+
+                     alert("Session expired. User successfully logged out.");
+                     window.location = "UserLogin.php";
+                 }
+
+             }, 1000);
+         }).mousemove();
+
+         var interval;
+                     $(document).on('keydown', function () {
+             clearInterval(interval);
+             var coutdown =1 * 60, $timer = $('.timer'); // After 5 minutes session expired (keyboard button press code)
+             $timer.text(coutdown);
+             interval = setInterval(function () {
+                 $timer.text(--coutdown);
+
+                 if (coutdown === 0) {
+
+                     alert("Session expired User successfully logout.");
+                     window.location = "UserLogin.php";
+                 }
+
+             }, 1000);
+         }).mousemove();
+   
